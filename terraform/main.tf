018d2f9780f129e44735ac95d3be4908d4acbea8
Emil <aws@cloudmaniak.pl>
2025-07-18 00:45:46 +0200

    feat: expose port 5044 for Logstash input

--- terraform/main.tf
+++ terraform/main.tf
@@ -90,6 +90,12 @@ resource "aws_security_group" "opensearch_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 5044
+    to_port     = 5044
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
